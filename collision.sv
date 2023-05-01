cd157fbdc4648c1bf32c8d5104db2820d1a65a3d
ZhuohaoXu <zhuohao6@illinois.edu>
2023-05-01 00:16:40 -0500

    Delete a little unnecessary code

--- collision.sv
+++ collision.sv
@@ -1,33 +1,3 @@
-//module collision_detection
-//(
-//input [9:0] x_0, x_1, y_0, y_1,
-//output ball0_win, ball1_win
-//);
-//
-////assign ball0_win = 0;
-////assign ball1_win = 0;
-//
-//always_comb
-//	begin
-//		if (((x_1 - x_0) < 8 & (x_1 - x_0) > 0)| ((x_0 - x_1) < 8 & (x_0 - x_1) > 0))
-//			if (y_0 - y_1 >= -16 & y_0 - y_1 <= 0)
-//				ball0_win = 1;
-//			else if (y_1 - y_0 >= -16 & y_1 - y_0 <= 0)
-//				ball1_win = 1;
-//			else
-//				begin
-//					ball0_win = 0;
-//					ball1_win = 0;
-//				end
-//		else
-//			begin
-//					ball0_win = 0;
-//					ball1_win = 0;
-//			end
-//	end
-//
-//	
-//endmodule
 module collision_detection
 (
 input [9:0] x_0, x_1, y_0, y_1,
@@ -54,7 +24,6 @@ always_comb
 
         if (((sx_0 - sx_1 > -16) & (sx_0 - sx_1 <= -8)) | ((sx_0 - sx_1 > -8) & (sx_0 - sx_1 <= 0)) | ((sx_0 - sx_1 > 0) & (sx_0 - sx_1 <= 8)) | ((sx_0 - sx_1 > 8) & (sx_0 - sx_1 <= 16)))
 			  begin
-	//			if (((x_0 - x_1 >= -16) & (x_0 - x_1 < 0)) | ((x_0 - x_1 > 0) & (x_0 - x_1 <= 16)))
 				  begin
 						if ((y_0 - y_1 >= -16) & (y_0 - y_1 <= -8))
 							 ball0_win = 1;
